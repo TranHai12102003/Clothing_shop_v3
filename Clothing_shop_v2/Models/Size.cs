2fd3be5a5a32615e8479987dc06f654b4a168efe
TranHai12102003 <trantrunghai141@gmail.com>
2025-05-09 21:59:27 +0700

    cập nhật APIs Size theo cấu trúc

--- Clothing_shop_v2/Models/Size.cs
+++ Clothing_shop_v2/Models/Size.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using Clothing_shop_v2.Common.Models;
 
 namespace Clothing_shop_v2.Models;
 
-public partial class Size
+public partial class Size : BaseEntity
 {
-    public int Id { get; set; }
+    //public int Id { get; set; }
 
     public string SizeName { get; set; } = null!;
 
-    public string? CreatedBy { get; set; }
+    //public string? CreatedBy { get; set; }
 
-    public string? UpdatedBy { get; set; }
+    //public string? UpdatedBy { get; set; }
 
-    public DateTime? CreatedDate { get; set; }
+    //public DateTime? CreatedDate { get; set; }
 
-    public DateTime? UpdatedDate { get; set; }
+    //public DateTime? UpdatedDate { get; set; }
 
-    public bool? IsActive { get; set; }
+    //public bool? IsActive { get; set; }
 
     public virtual ICollection<Variant> Variants { get; set; } = new List<Variant>();
 }
